ff7a79456d37aecdfee80ccb627239dfca6b0f0c
vinay <vinay.yadav_cs19@gla.ac.in>
2021-02-10 10:58:15 +0530

    initial commit

---
+++ src/implementation/MyLinkedList.java
@@ -0,0 +1,60 @@
+package implementation;
+
+import myinterface.MySinglyLinkedList;
+
+public class MyLinkedList  implements MySinglyLinkedList {
+    // head reference or start of linked list or reference of first node
+    private Node head;
+    // tail reference or end of linked list or reference of last node
+    private Node tail;
+    // store total number of elements in linked list
+    private int size;
+
+    //constructor
+
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    @Override
+    public void addFirst(int element) {
+        Node node = new Node(element);
+        if (isEmpty()) {
+            head = node;
+            tail = node;
+        } else {
+            node.setNext(head);
+            head = node;
+            size++;
+        }
+    }
+
+    // traverse
+    public void traverse() {
+        if (!isEmpty()) {
+            Node temp = head;
+            while(temp != null){
+                System.out.println(temp.getData()+ "-->");
+                // updating temp value to refer next node of sequence
+                temp = temp.getNext();
+            }
+            System.out.println("null");
+        }
+        else {
+            System.out.println("linked list empty");
+        }
+    }
+}
---
+++ src/main/MyMain.java
@@ -0,0 +1,16 @@
+package main;
+
+import implementation.MyLinkedList;
+
+public class MyMain {
+    public static void main(String[] args) {
+        MyLinkedList linkedList = new MyLinkedList();
+        linkedList.addFirst(500);
+        linkedList.traverse();
+        linkedList.addFirst(700);
+        linkedList.addFirst(200);
+        linkedList.traverse();
+        System.out.print("size =" + linkedList.size());
+    }
+
+}
